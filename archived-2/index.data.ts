5e50de1f432a56fb4265d9be5ce069a0483e2d21
jdrydn <james@jdrydn.com>
2026-04-03 20:05:35 +0100

    refactor: Archived (1/2)

--- archived-2/index.data.ts
+++ archived-2/index.data.ts
@@ -1,13 +1,13 @@
 import path from 'path';
 import { createContentLoader } from 'vitepress';
 
-export default createContentLoader(path.resolve(__dirname, '../Archived/*.md'), {
+export default createContentLoader(path.resolve(__dirname, '../archived/*.md'), {
   includeSrc: false,
   render: false,
   excerpt: true,
   transform: (items) =>
     items
-      .filter((item) => item.url !== '/Archived/')
+      .filter((item) => item.url !== '/archived/')
       .map((item) => {
         const defaultTitle = path.basename(item.url, path.extname(item.url));
         item.frontmatter.title = item.frontmatter.title || defaultTitle;
