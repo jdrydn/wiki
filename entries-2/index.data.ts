9f0f26bc391e7f15d74e028d46d936829db52341
jdrydn <james@jdrydn.com>
2026-04-03 20:07:45 +0100

    refactor: Entries (1/2)

--- entries-2/index.data.ts
+++ entries-2/index.data.ts
@@ -1,13 +1,13 @@
 import path from 'path';
 import { createContentLoader } from 'vitepress';
 
-export default createContentLoader(path.resolve(__dirname, '../Entries/*.md'), {
+export default createContentLoader(path.resolve(__dirname, '../entries/*.md'), {
   includeSrc: false,
   render: false,
   excerpt: true,
   transform: (items) =>
     items
-      .filter((item) => item.url !== '/Entries/')
+      .filter((item) => item.url !== '/entries/')
       .map((item) => {
         const defaultTitle = path.basename(item.url, path.extname(item.url));
         item.frontmatter.title = item.frontmatter.title || defaultTitle;
